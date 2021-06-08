a8c664bac4bfbc5915ec3adc559f0d35f3a0f80b
Ryan Tomlinson <35232317+rtomlinson1@users.noreply.github.com>
2021-06-08 17:37:54 -0500

    preliminary text/styling

--- laravel/resources/views/pages/index.blade.php
+++ laravel/resources/views/pages/index.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.page')
 
 @section('content')
+<div class="">
+  <h1>How are you marketing <span id="marketing-items" class="standout">yourself?</span></h1>
+  <p>We exist to help you market <span class="standout">smarter</span> not harder.</p>
 
-<h1>Test Index</h1>
+  <h3>Where do I start?</h3>
+  <p>Developing a <span class="standout">brand</span> with a distinct voice is not something that is accomplished without planning and organization. At Smart Marketing, we believe a great starting point is with your <span class="standout">website.</span> Our process of building or revamping your web presence will encourage you to take time to think about what you are all about. We will work with you to collect the information your clients will be looking for, and present it in a way that is unique to your brand's <span class="standout">personality.</span> </p>
+</div>
 @endsection
