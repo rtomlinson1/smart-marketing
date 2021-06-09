bb86168c4a6223c2395c78704c3ee251324a3a52
Ryan Tomlinson <35232317+rtomlinson1@users.noreply.github.com>
2021-06-09 10:05:03 -0500

    added copy to homepage

--- laravel/resources/views/pages/index.blade.php
+++ laravel/resources/views/pages/index.blade.php
@@ -5,7 +5,20 @@
   <h1>How are you marketing <span id="marketing-items" class="standout">yourself?</span></h1>
   <p>We exist to help you market <span class="standout">smarter</span> not harder.</p>
 
-  <h3>Where do I start?</h3>
+  <h2>Where do I start?</h2>
   <p>Developing a <span class="standout">brand</span> with a distinct voice is not something that is accomplished without planning and organization. At Smart Marketing, we believe a great starting point is with your <span class="standout">website.</span> Our process of building or revamping your web presence will encourage you to take time to think about what you are all about. We will work with you to collect the information your clients will be looking for, and present it in a way that is unique to your brand's <span class="standout">personality.</span> </p>
+
+  <h3>Tell your story. Introduce yourself. Get recognized.</h3>
+  <p>In the process of building your web presence, the most important step is deciding or defining <span class="standout">who you are</span> as a company. This determines how you will brand everything from a blog post to a print piece. This "voice" for your company is how you can stay "on brand" across all your marketing efforts. It's more than just the words you say and how you say them. Your brand is the photos you use, your logo, colors, design, and other companies and people you associate with your brand. Your brand has to be clear, memorable, united, and protected. It is the way customers identify you, and influences how they feel about you.</p>
+  <p>You are more than a logo, or a product. You are an influence. A perpective. An identity. It's time to show it.</p>
+
+  <h2>What's next?</h2>
+  <p>Once your brand is defined, and your web presence adheres to that brand, it's time to start advertising. We are a digital marketing agency, so let's start there.</p>
+  <p>How are your <strong>social accounts</strong> doing? Your branding should extend here as well, determinig which social platforms you focus on. Facebook? Twitter? Youtube? We will apply your <span class="standout">business goals</span> to your <span class="standout">target audience</span> - important things we will help you identify during that branding process we talked about earlier. From there we can target that audience with advertisements that are crafted for your audience and show to your audience.</p>
+  <p>What if you dont have an internal marketing team? <span class="standout">We've got you covered.</span> When you bring Smart Marketing on, you're hiring a group of specialists. Need <strong>graphic design?</strong> We can create custom graphics for social or print -anything from business cards to sermon slides to a pitch deck. We will work within your guidelines and keep it on brand and on budget.</p>
+  <p>Targeting ads toward an audience is a great first step, but what about long term growth? Our <strong>Search Engine Optimization</strong> services will take the website you already have, evaluate where people are currently finding you and your direct competitors, and find ways to <span class="standout">make you show up</span> in those searches. This is a long game, not an overnight change, so prepare for slow, sustained progress. Remember, work you do here will draw in business long after an ad campaign is over. For that reason, it's best to take a multi-pronged approach, focusing some ad budget on targeted ads, and some on <strong>SEO.</strong></p>
+  <p>If a picture is worth a thousand words, how many is a video worth? When you need <strong>video production</strong>, our team has the gear and experience to capture your event, create training videos or <span class="standout">anything else you can think of.</span> If you aren't near our area, we will set you up with one of our trusted nationwide partners and make sure they understand both your brand and your needs.</p>
+  <p>What if your business has goals that aren't sales or profits? Smart Marketing works with more than a dozen churches and non-profits on everything from website hosting to social media management. Even if you rely on donations rather than sales, you still need a brand that people can find, identify with, and get excited about. Check out what we did for First Baptist Church of Poplar Bluff, MO. </p>
+  <a class="primary-button" href="#">Learn More</a>
 </div>
 @endsection
