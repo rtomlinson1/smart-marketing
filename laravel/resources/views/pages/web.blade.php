f20a3d880bdec5024293d7a4af6052edda5ff749
Ryan Tomlinson <35232317+rtomlinson1@users.noreply.github.com>
2022-01-07 16:48:12 -0600

    fixed imac size and position

--- laravel/resources/views/pages/web.blade.php
+++ laravel/resources/views/pages/web.blade.php
@@ -83,25 +83,25 @@
 
 </div>
 <script>
-		// init
-		// var controller = new ScrollMagic.Controller();
-    //
-		// // define movement of panels
-		// var wipeAnimation = new TimelineMax()
-		// 	.fromTo("section.panel.turqoise", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})  // in from left
-		// 	.fromTo("section.panel.green",    1, {x:  "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
-		// 	.fromTo("section.panel.bordeaux", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone}); // in from top
-    //
-		// // create scene to pin and link animation
-		// new ScrollMagic.Scene({
-		// 		triggerElement: "#pinContainer",
-		// 		triggerHook: "onLeave",
-		// 		duration: "1000%"
-		// 	})
-		// 	.setPin("#pinContainer")
-		// 	.setTween(wipeAnimation)
-		// 	.addIndicators() // add indicators (requires plugin)
-		// 	.addTo(controller);
+		init
+		var controller = new ScrollMagic.Controller();
+
+		// define movement of panels
+		var wipeAnimation = new TimelineMax()
+			.fromTo("section.panel.turqoise", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})  // in from left
+			.fromTo("section.panel.green",    1, {x:  "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
+			.fromTo("section.panel.bordeaux", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone}); // in from top
+
+		// create scene to pin and link animation
+		new ScrollMagic.Scene({
+				triggerElement: "#pinContainer",
+				triggerHook: "onLeave",
+				duration: "1000%"
+			})
+			.setPin("#pinContainer")
+			.setTween(wipeAnimation)
+			.addIndicators() // add indicators (requires plugin)
+			.addTo(controller);
 
 
 </script>
