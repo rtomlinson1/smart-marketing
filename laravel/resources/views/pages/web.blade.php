bb46cd3c5e2a3cbadb86e8dc08868db006ea5883
Ryan Tomlinson <35232317+rtomlinson1@users.noreply.github.com>
2022-01-13 11:44:53 -0600

    fix web page

--- laravel/resources/views/pages/web.blade.php
+++ laravel/resources/views/pages/web.blade.php
@@ -30,7 +30,7 @@
   </section>
   <section class="is-100">
     <div class="is-flex align-top ">
-      <div class="imac-outer-wrap is-desktop-only">
+      <div class="imac-outer-wrap">
         <div class="background-cover">
           <div class="imac-wrap">
             <div class="imac">
@@ -49,7 +49,7 @@
 
         <div class="southern-bank">
           <h1>Southern Bank</h1>
-          <img src="/images/websites/southern-mobile.jpg" alt="">
+          <img class="mobile-image" src="/images/websites/southern-mobile.jpg" alt="">
           <h3>Core Objective:</h3>
           <p>Smart Marketing was tasked with rebuilding Southern Bank’s entire client-side web presence. Southern Bank was refreshing their branding from the ground up, with new products, a new tone, and even some new colors -- all while maintaining their core values as a local bank with big possibilities.</p>
           <h3>Our Part:</h3>
@@ -59,21 +59,33 @@
           <h3>What we did:</h3>
           <p>Wherever a client lands on bankwithsouthern.com, they are guided to the information they need to make a decision. Each page has a purpose; each call to action has a goal. The current site is a showcase of what can happen when a web presence is designed to answer questions, not just to look impressive. We are proud of the part we played in bringing Southern’s new branding to the web. We believe the site is beautiful, functional, secure and able to convey the ideals of Southern Bank to their current and potential clients.</p>
         </div>
-        <div class="manulife">
-          <h1>Manulife Parking App</h1>
-          <img src="/images/websites/manulife.jpg" alt="">
-          <h3>Core Objective:</h3>
-          <p>Manufacturer’s Life Insurance Company (Manulife) has a large parking area at their main campus. They partnered with HubParking to install parking lot sensors and signage in their parking garage and open parking areas. These sensors allow customers and employees to see parking availability as they drive onto the campus. Through our partnership with Indect (the manufacturer of the sensors) and HubParking (the implementation partner for the signage and sensors), our task was to create a website that could retrieve data from the parking sensors, and display it live for people who are planning to visit.</p>
 
-          <h3>Technical Challenges:</h3>
-          <p>The application needed to be very fast loading, look good and provide all of the parking lot information on an area that fits on a single phone screen. The information needed to automatically refresh on a periodic basis. Finally, we needed to create a map that provided the location of each lot and sub-lot in a user friendly way. </p>
-          <h3>What we did:</h3>
-          <p>Working directly with our partner Indect, we were able to interface with the parking lot server and retrieve live data from it. This raw data gave us the total number of spaces for each section and sub-section, as well as the total number of occupied spaces. From this data, we were able to calculate the available spaces and overlay that information onto a single phone screen.</p>
-          <p>We started with a design that fit with Manulife’s current branding. We went through iterations of this design to come up with a way to show all the data on small devices. We used a series of “progress bar” styled meters to show how full each area was, with an updating number count displayed above the line. We then translated the paper chart of shuttle times into an easily readable mobile-friendly table. Finally we created a sharp, branded graphic to show guests how to get to each lot, and added links to get directions and to download the pdf versions of the map and shuttle schedule.</p>
-          <div class="text-link not-black">
-            <a href="/social"><strong>See More Projects ></strong></a>
-          </div>
+      </div>
+
+
+
+
+    </div>
+  </section>
+  <section>
+    <div class="is-flex">
+      <div class="manulife">
+        <h1>Manulife Parking App</h1>
+        <img class="mobile-image" src="/images/websites/manulife.jpg" alt="">
+        <h3>Core Objective:</h3>
+        <p>Manufacturer’s Life Insurance Company (Manulife) has a large parking area at their main campus. They partnered with HubParking to install parking lot sensors and signage in their parking garage and open parking areas. These sensors allow customers and employees to see parking availability as they drive onto the campus. Through our partnership with Indect (the manufacturer of the sensors) and HubParking (the implementation partner for the signage and sensors), our task was to create a website that could retrieve data from the parking sensors, and display it live for people who are planning to visit.</p>
+
+        <h3>Technical Challenges:</h3>
+        <p>The application needed to be very fast loading, look good and provide all of the parking lot information on an area that fits on a single phone screen. The information needed to automatically refresh on a periodic basis. Finally, we needed to create a map that provided the location of each lot and sub-lot in a user friendly way. </p>
+        <h3>What we did:</h3>
+        <p>Working directly with our partner Indect, we were able to interface with the parking lot server and retrieve live data from it. This raw data gave us the total number of spaces for each section and sub-section, as well as the total number of occupied spaces. From this data, we were able to calculate the available spaces and overlay that information onto a single phone screen.</p>
+        <p>We started with a design that fit with Manulife’s current branding. We went through iterations of this design to come up with a way to show all the data on small devices. We used a series of “progress bar” styled meters to show how full each area was, with an updating number count displayed above the line. We then translated the paper chart of shuttle times into an easily readable mobile-friendly table. Finally we created a sharp, branded graphic to show guests how to get to each lot, and added links to get directions and to download the pdf versions of the map and shuttle schedule.</p>
+        <div class="text-link not-black">
+          <a href="/social"><strong>See More Projects ></strong></a>
         </div>
+      </div>
+      <div class="">
+
       </div>
     </div>
   </section>
